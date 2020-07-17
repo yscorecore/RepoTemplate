c92ba16a930a1f7c73e1146169457a1dd568e440
pengbo.yang <obpt@sina.com>
2020-07-17 19:55:31 +0800

    revert github package source

--- .github/scripts/pushpack.sh
+++ .github/scripts/pushpack.sh
@@ -3,5 +3,5 @@ PUBLISH_FOLDER=$1
 NUGET_APIKEY=$2
 for file in `ls $PUBLISH_FOLDER/*.nupkg`
 do
-    dotnet nuget push $file -k $NUGET_APIKEY -s  https://api.nuget.org/v3/index.json --skip-duplicate --source "github"
+    dotnet nuget push $file -k $NUGET_APIKEY -s  https://api.nuget.org/v3/index.json --skip-duplicate
 done
