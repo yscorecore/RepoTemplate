a5ca84f8cac3f18613748525e4eebae405c8c451
pengbo.yang <obpt@sina.com>
2021-08-06 00:18:45 +0800

    upgrade to net5

--- test/YS.Hello.UnitTest/UnitTest1.cs
+++ test/YS.Hello.UnitTest/UnitTest1.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿
+using FluentAssertions;
+using Xunit;
 
 namespace Hello.UnitTest
 {
-    [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
+        [Fact]
         public void TestMethod1()
         {
+            "abc".Should().Be("abc");
         }
+        
     }
 }
